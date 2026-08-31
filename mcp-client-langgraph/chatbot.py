66892ca6bd5827d3d664bc13a71c6cf8d99cdc91
Saurav kumar <sauravtiwari2712005@gmail.com>
2026-08-31 22:55:42 +0530

    Refactor chatbot.py to improve structure and clarity

--- mcp-client-langgraph/chatbot.py
+++ mcp-client-langgraph/chatbot.py
@@ -11,7 +11,7 @@ from langchain_core.tools import tool
 load_dotenv() 
 llm = ChatOpenAI(model="gpt-5")
 
-# user deifne tool
+
 @tool
 def calculator(first_num: float, second_num: float, operation: str) -> dict:
     """
@@ -40,11 +40,11 @@ tools = [calculator]
 
 llm_with_tools = llm.bind_tools(tools)
 
-# state
+
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
-# nodes
+
 def chat_node(state: ChatState):
 
     messages = state["messages"]
@@ -53,7 +53,7 @@ def chat_node(state: ChatState):
 
 tool_node = ToolNode(tools)
 
-# defining graph and nodes
+
 graph = StateGraph(ChatState)
 
 graph.add_node("chat_node", chat_node)
